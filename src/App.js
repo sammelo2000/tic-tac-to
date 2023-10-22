188fa51548a4913d2d639128d275f6418976e912
SAMUEL WILLIAMS <108836524+sammelo2000@users.noreply.github.com>
2023-10-22 02:33:07 +0200

    showing past moves

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import { useState} from 'react';
 
 
+export default function Game() {
+
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+  
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
+
+
 function Square({value,onSquareClick}) {
   return (
    <button className="square" onClick={onSquareClick}>
@@ -12,9 +35,8 @@ function Square({value,onSquareClick}) {
  }
 
 
-export default function Board() {
-  const [squares,setSquares] = useState((Array(9).fill(null)));
-  const [xIsNext,setXIsNext] = useState(true);
+ function Board({xIsNext, squares, onPlay}) {
+  
   
   
   
@@ -29,8 +51,7 @@ export default function Board() {
     }else {
       nextSquares[i] = 'O';
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
